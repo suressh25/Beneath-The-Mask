abb4fab11b411257df79a78410b66d756c76c9bb
Nadeem <m.nadeem83005@gmail.com>
2023-02-19 13:37:57 +0530

    Added Pyotp - Reformat

--- website/auth.py
+++ website/auth.py
@@ -1,53 +1,74 @@
-from flask import Blueprint,render_template,request,redirect,url_for,flash
-from flask_login import login_required,logout_user,current_user
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_required, logout_user, current_user
 from . import db
-auth=Blueprint("auth",__name__)
+import pyotp
 
-@auth.route("/login/",methods=["POST","GET"])
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login/", methods=["POST", "GET"])
 @login_required
 def login_page():
-	if request.method=="POST":
-		if request.form.get("username")=="bruh" and request.form.get("password") =="bruh@123":
-			flash("password_correct","success")
-			current_user.ispassword=True
-			db.session.commit()
-			return redirect(url_for("auth.security"))
-		return redirect(url_for("auth.login_page"))
-	return render_template("login.html")
+    if request.method == "POST":
+        if request.form.get("username") == "bruh" and request.form.get("password") == "bruh@123":
+            flash("password_correct", "success")
+            current_user.ispassword = True
+            db.session.commit()
+            return redirect(url_for("auth.security"))
+        return redirect(url_for("auth.login_page"))
+    return render_template("login.html")
 
 
-@auth.route("/security/",methods=["POST","GET"])
+@auth.route("/security/", methods=["POST", "GET"])
 @login_required
 def security():
-	if request.method=="POST":
-		creds = {"Catname": "ALEX", "Hometown": "MADURAI","Food":"PIZZA"}
-		Catname=request.form.get("Catname")
-		Hometown=request.form.get("Hometown")
-		Food=request.form.get("Food")
-		if Catname.upper() == creds["Catname"] and Hometown.upper() == creds["Hometown"] and Food.upper() == creds["Food"]:
-			flash("you have answered the security question correctly","success")
-			current_user.issecurityquestion=True
-			db.session.commit()
-			return redirect(url_for("auth.twofactor"))
-		else:
-			flash("Wrong Credentials","danger")
-			return redirect(url_for("auth.security"))
-	if current_user.ispassword==0:
-		flash("Not authorized","danger")
-		return redirect(url_for("auth.login_page"))
-	return render_template("login_security.html")
+    if request.method == "POST":
+        creds = {"Catname": "ALEX", "Hometown": "MADURAI", "Food": "PIZZA"}
+        Catname = request.form.get("Catname")
+        Hometown = request.form.get("Hometown")
+        Food = request.form.get("Food")
+        if Catname.upper() == creds["Catname"] and Hometown.upper() == creds["Hometown"] and Food.upper() == creds[
+            "Food"]:
+            flash("you have answered the security question correctly", "success")
+            current_user.issecurityquestion = True
+            db.session.commit()
+            return redirect(url_for("auth.twofactor"))
+        else:
+            flash("Wrong Credentials", "danger")
+            return redirect(url_for("auth.security"))
+    if current_user.ispassword == 0:
+        flash("Not authorized", "danger")
+        return redirect(url_for("auth.login_page"))
+    return render_template("login_security.html")
 
 
 @auth.route("/logout/")
 @login_required
 def logout():
-	logout_user()
-	return redirect(url_for("views.home_page"))
+    logout_user()
+    return redirect(url_for("views.home_page"))
 
-@auth.route("/twofactor",methods=["POST","GET"])
+
+@auth.route("/last_page/")
+@login_required
+def last_page():
+    return render_template("last_page.html")
+
+
+@auth.route("/twofactor", methods=["POST", "GET"])
 @login_required
 def twofactor():
-	if current_user.issecurityquestion==0:
-		flash("Not authorized","danger")
-		return redirect(url_for("auth.security"))
-	return render_template("login_2fa.html")
+    if request.method == "POST":
+        otp = int(request.form.get("otp"))
+        if pyotp.TOTP("JBSWY3DPEHPK3PXP").verify(otp):
+            current_user.isofa = True
+            db.session.commit()
+            flash("The TOTP 2FA token is valid", "success")
+            return redirect(url_for("auth.last_page"))
+        else:
+            flash("You have supplied an invalid 2FA token!", "danger")
+            return redirect(url_for("auth.twofactor"))
+    if current_user.issecurityquestion == 0:
+        flash("Not authorized", "danger")
+        return redirect(url_for("auth.security"))
+    return render_template("login_2fa.html")
--- website/views.py
+++ website/views.py
@@ -1,44 +1,49 @@
-from flask import Blueprint,render_template,request,flash,redirect,url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_user
-from .  import db
+from . import db
 from .modals import User
-views= Blueprint("views",__name__)
 
-@views.route("/",methods=["POST","GET"])
+views = Blueprint("views", __name__)
+
+
+@views.route("/", methods=["POST", "GET"])
 def home_page():
-	if request.method =="POST":
-		name=request.form.get("username")
-		teamname=request.form.get("teamname")
-		if User.query.filter_by(username=name).first():
-			flash("Username already exits!","danger")
-		else:
-			flash("Successfully created!","success")
-			new_user=User(username=name,teamname=name,ispassword=0,issecurityquestion=0,isofa=0)
-			db.session.add(new_user)
-			db.session.commit()
-			login_user(new_user)
-			return redirect(url_for("auth.login_page"))
-	return render_template("home_page.html")
+    if request.method == "POST":
+        name = request.form.get("username")
+        teamname = request.form.get("teamname")
+        if User.query.filter_by(username=name).first():
+            flash("Username already exits!", "danger")
+        else:
+            flash("Successfully created!", "success")
+            new_user = User(username=name, teamname=name, ispassword=0, issecurityquestion=0, isofa=0)
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for("auth.login_page"))
+    return render_template("home_page.html")
+
 
 @views.route("/dev-login/<user_id>/<password>")
-def dev_login(user_id,password):
-	if password!="$admin$":
-		return redirect(url_for("views.home_page"))
-	user=User.query.filter_by(username=user_id).first()
-	login_user(user)
-	return redirect(url_for("auth.login_page"))
+def dev_login(user_id, password):
+    if password != "$admin$":
+        return redirect(url_for("views.home_page"))
+    user = User.query.filter_by(username=user_id).first()
+    login_user(user)
+    return redirect(url_for("auth.login_page"))
+
 
 @views.route("/dev-delete/<user_id>/<password>")
-def dev_delete(user_id,password):
-	if password!="$admin$":
-		return redirect(url_for("views.home_page"))
-	User.query.filter_by(username=user_id).delete()
-	db.session.commit()
-	return redirect(url_for("views.home_page"))
+def dev_delete(user_id, password):
+    if password != "$admin$":
+        return redirect(url_for("views.home_page"))
+    User.query.filter_by(username=user_id).delete()
+    db.session.commit()
+    return redirect(url_for("views.home_page"))
+
 
 @views.route("/dev-dashboard/<password>")
 def dev_dashboard(password):
-	if password!="$admin$":
-		return redirect(url_for("views.home_page"))
-	lst=User.query.all()
-	return render_template("dashboard.html",users=lst)
+    if password != "$admin$":
+        return redirect(url_for("views.home_page"))
+    lst = User.query.all()
+    return render_template("dashboard.html", users=lst)
